5ffd3ade87840840b816bbe1ba3aaa94625da6e5
Vasko <vasko5petrov@gmail.com>
2018-08-10 09:05:19 +0300

    fixed indentation

--- js/script.js
+++ js/script.js
@@ -1,50 +1,50 @@
 var buttons = document.querySelectorAll('#calculator-wrapper button');
-        var input = document.getElementById('screen');
-        var actions = ['+', '-', 'x', '/'];
+var input = document.getElementById('screen');
+var actions = ['+', '-', 'x', '/'];
 
-        var isDecimal = false;
+var isDecimal = false;
         
-        buttons.forEach(function(button) {
-            button.addEventListener('click', function(e) {
+buttons.forEach(function(button) {
+    button.addEventListener('click', function(e) {
                 
-                e.preventDefault();
+        e.preventDefault();
                 
-                var buttonValue = e.target.innerHTML;
+        var buttonValue = e.target.innerHTML;
                 
-                if(buttonValue == "C") {
-                    input.innerHTML = "";
-                    isDecimal = false;
-                } else if(buttonValue == "=") {
-                    var toSolve = input.innerHTML;
-                    var lastChar = toSolve[toSolve.length - 1];
+        if(buttonValue == "C") {
+            input.innerHTML = "";
+            isDecimal = false;
+        } else if(buttonValue == "=") {
+            var toSolve = input.innerHTML;
+            var lastChar = toSolve[toSolve.length - 1];
 
-                    toSolve = toSolve.replace(/x/g, '*');
+            toSolve = toSolve.replace(/x/g, '*');
 
-                    if(actions.indexOf(lastChar) > -1 || lastChar == ".") {
-                        toSolve = toSolve.replace(/.$/, '');
-                    }
+            if(actions.indexOf(lastChar) > -1 || lastChar == ".") {
+                toSolve = toSolve.replace(/.$/, '');
+            }
 
-                    if(toSolve) {
-                        input.innerHTML = eval(toSolve);
-                    }
+            if(toSolve) {
+                input.innerHTML = eval(toSolve);
+            }
 
-                    isDecimal = false;
-                } else if(actions.indexOf(buttonValue) > -1) {
-                    var lastChar = input.innerHTML[input.innerHTML.length - 1];
+            isDecimal = false;
+        } else if(actions.indexOf(buttonValue) > -1) {
+            var lastChar = input.innerHTML[input.innerHTML.length - 1];
 
-                    if(input.innerHTML != '' && actions.indexOf(lastChar) == -1) {
-                        input.innerHTML += buttonValue;
-                    }
+            if(input.innerHTML != '' && actions.indexOf(lastChar) == -1) {
+                input.innerHTML += buttonValue;
+            }
 
-                    isDecimal = false;
-                } else if(buttonValue == '.') {
-                    if(!isDecimal) {
+            isDecimal = false;
+        } else if(buttonValue == '.') {
+            if(!isDecimal) {
                         input.innerHTML += buttonValue;
                         isDecimal = true;
-                    }
-                } else {
-                    input.innerHTML += buttonValue;
-                }
+            }
+        } else {
+            input.innerHTML += buttonValue;
+        }
                 
-            });
-        });
+    });
+});
